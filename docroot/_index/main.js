ea945ef9c6c7ad0107d90cbdcc5e2c39178fbb71
Federico Turbino <f.turbino@liguriadigitale.it>
2025-07-24 12:09:20 +0200

    That piece of code was actually useless, there was no breadcrumb to begin with

--- docroot/_index/main.js
+++ docroot/_index/main.js
@@ -1,28 +1,2 @@
 var contentTable = document.getElementById('list');
 contentTable.setAttribute('class', 'table table-hover table-transparent-bg');
-
-var currentDirectory = location.protocol + "//" + location.host + "/";
-var directoryText = document.getElementById('nginx-fancyindex-directory').textContent || document.getElementById('nginx-fancyindex-directory').innerText;
-var directories = directoryText.split('/');
-var breadcrumbsElement = document.getElementById('breadcrumbs');
-var i, directory, listItem, link;
-
-for (i = 0; i < directories.length; i++) {
-  directory = directories[i];
-  listItem = document.createElement('li');
-  listItem.className = 'breadcrumb-item';
-  
-  link = document.createElement('a');
-  link.href = currentDirectory;
-  
-  if (i === 0) {
-    link.textContent = '(root)';
-  } else {
-    link.href = currentDirectory + directory + '/';
-    link.textContent = directory;
-    currentDirectory = currentDirectory + directory + '/';
-  }
-  
-  listItem.appendChild(link);
-  breadcrumbsElement.appendChild(listItem);
-}
